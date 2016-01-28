63a06d085731dbac083bda490bb6e699f927b39d
artem <artem@gypost.com>
2016-01-28 19:46:09 +0200

    clear cache service

--- app/src/common/cacheService.js
+++ app/src/common/cacheService.js
@@ -1,20 +1,19 @@
 /**
  * Created by askuznetsov on 1/14/2016.
+ * TODO: only skeleton for testing; refactor needs;
  */
 
-angular.module('foodApp.modules.common.services.cache', [])
+angular.module('foodApp.modules.common.services.localCache', [])
 
-    .factory('cacheService', ['$cacheFactory', function($cacheFactory) {
+    .factory('localCacheService', [function() {
         // TODO: cache map, setter, clear time, so on
         return {
             get : function(cacheKey) {
-                return $cacheFactory(cacheKey);
+                return cacheKey;
             },
             put : function(key, value) {
-                var cached = $cacheFactory(key);
-                cached.put(key, value);
-                return value;
+                return key;
             }
         }
-    }])
+    }]);
     // TODO: storages, cache - browser, file, cookie;
