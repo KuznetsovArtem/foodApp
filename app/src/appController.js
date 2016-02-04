b426ed7db7ad3bb3af32e47a912a28987570758b
441N143G <dilenyan@gmail.com>
2016-02-04 18:59:23 +0200

    kakashka

--- app/src/appController.js
+++ app/src/appController.js
@@ -2,13 +2,13 @@
  * Created by askuznetsov on 12/23/2015.
  */
 angular.module('foodApp')
-    .controller('appController', ['$scope', 'localCacheService', function ($scope, cache) {
+    .controller('appController', ['$scope', 'localCacheService', '$element', function ($scope, cache, $element) {
         ons.bootstrap();
 
 
         // TODO: rm: next lines for education purposes;
-        cache.put('some-data', {a:1});
-        cache.put('some-data2', {b:1, c: 100}, {
+        cache.put('some-data', {a: 1});
+        cache.put('some-data2', {b: 1, c: 100}, {
             expires: 5 * 60 * 1000,
             protected: false,
             autoClear: true
@@ -20,24 +20,32 @@ angular.module('foodApp')
 
 
         $scope.loading = {
-            showWeirdLoad : false
+            showWeirdLoad: false
         };
-
 
 
         // TODO: move to own provider/service
         var CONFIG = {
-            leftBar : {
+            leftBar: {
                 isOpen: false,
                 //here will be list with animates, u'll setUp what u want in 'anime'
                 animation: 'slide',
                 //don't touch this plz
                 style: {'width': '40vw'}
             },
-            descriptionDirective : {
+            descriptionDirective: {
                 symbolNumber: '180'
             }
         };
 
         $scope.CONFIG = CONFIG;
+        var res = '';
+        $scope.slider = function () {
+                if (CONFIG.leftBar.isOpen) {
+                    res = 'slideMainContent';
+                } else if(res === 'slideMainContent') {
+                    res = 'slideMainContentBack';
+                }
+            return res;
+        }
     }]);
